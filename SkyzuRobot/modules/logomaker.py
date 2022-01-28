13df09db0ba6024cbd9c5c4f7251accc18825259
DoniRamadhan <95742544+Donigmg@users.noreply.github.com>
2022-01-28 16:39:19 +0700

    Update logomaker.py

--- SkyzuRobot/modules/logomaker.py
+++ SkyzuRobot/modules/logomaker.py
@@ -198,7 +198,7 @@ async def logo_(event):
         await tbot.send_file(
             event.chat_id,
             file=flnme,
-            caption="Logo by [Skyzu Robot](https://t.me/SkyzuRobot)",
+            caption="Logo by [Syn Robot](https://t.me/synxrobot)",
             force_document=False,
         )
         os.remove(flnme)
